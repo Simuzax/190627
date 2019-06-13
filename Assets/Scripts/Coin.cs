f27aea9880ebe015dcda5bca5827568ee3a71d5c
Simuzax <simuzax@gmail.com>
2019-06-13 10:54:05 -0300

    Commit 0613

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    float rotationSpeed = 180;
+    float degrees = 90;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 to = new Vector3(degrees, 0, 0);
+
+        transform.eulerAngles = to;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+    }
+}
